d44fbcd2fe54ab2f5b497ecee51a9dfef7fe1019
vkrishnajayanthi <vkrishnajayanthi>
2016-11-23 11:46:04 -0500

    Using new Level creation

--- Game1/Levels/Level.cs
+++ Game1/Levels/Level.cs
@@ -16,47 +16,12 @@ namespace Game1.Levels
             // Nothing as of now..
         }
 
-        public override void Render()
+        public override void Tick()
         {
-            base.Render();
+            base.Tick();
 
-            for (int x = 0; x < this.Width; x++)
-            {
-                for (int y = 0; y < this.Height; y++)
-                {
-                    RectangleF source;
-                    // Tileset has 5 different tiles. Each starting from 1 to 5.
-                    // Their location on the sprite sheet 
-                    switch (this[x, y].Type)
-                    {
-                        case 1:
-                            source = GetSourceRectangle(0, 0, Constants.TILE_SIZE);
-                            break;
 
-                        case 2:
-                            source = GetSourceRectangle(0, 1, Constants.TILE_SIZE);
-                            break;
-
-                        case 3:
-                            source = GetSourceRectangle(0, 2, Constants.TILE_SIZE);
-                            break;
-
-                        case 4:
-                            source = GetSourceRectangle(0, 3, Constants.TILE_SIZE);
-                            break;
-
-                        case 5:
-                            source = GetSourceRectangle(1, 0, Constants.TILE_SIZE);
-                            break;
-
-                        default:
-                            source = new RectangleF(0, 0, 0, 0);
-                            break;
-                    }
-
-                    DrawSprite(source, x, y);
-                }
-            }
         }
+
     }
 }
