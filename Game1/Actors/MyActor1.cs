7566385631591915fca38f9525bd9ffe7936acbf
vkrishnajayanthi <vkrishnajayanthi>
2016-11-24 22:36:18 -0500

    Refactored ActorPhysics

--- Game1/Actors/MyActor1.cs
+++ Game1/Actors/MyActor1.cs
@@ -20,11 +20,8 @@ namespace Game1.Actors
         {
             base.onHit(otherActor);
 
-
             this.Velocity *= -1f;
-
-            for(int i = 0; i < 20; ++i)
-                this.Position += this.Velocity;
+            this.Position += this.Velocity;
         }
     }
 }
